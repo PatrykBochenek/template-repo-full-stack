bd64fafdc3bc6dcce8f9919c9336f54306894967
Patryk Bochenek <pb339@protonmail.com>
2025-01-22 22:57:39 +0000

    fix (urls): Adjust the url structures

--- api/clients/urls.py
+++ api/clients/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("clients", views.get_all_clients, name="index"),
-    path("client-account/<int:client_id>", views.get_client_account_periods, name="client.account-period")
+    path("clients/", views.get_all_clients, name="clients.list"),
+    path(
+        "clients/<int:client_id>/accounting-periods/",
+        views.get_client_account_periods,
+        name="client.account-periods"
+    ),
 ]
